99b01d4976b3db1d46b0e093eef557b84b8e3b88
Ishita Mengi <mengiishita@gmail.com>
2023-07-25 17:12:35 +0200

    Defining routes using react-router and defining corresponding components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from "react";
 import AppContext from "./context/contextApi";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Feed from "./components/Feed";
+import SearchResults from "./components/SearchResults";
+import VideoDetails from "./components/VideoDetails";
 
 const App = () => {
   return (
     <AppContext>
-      <div>App</div>
+      <Router>
+        <div className="flex flex-col h-full">
+            <Header />
+            <Routes>
+                <Route path="/" element={<Feed />} exact/>
+                <Route path="/searchResults/:searchQuery" element={<SearchResults />}/>
+                <Route path="/video/:id" element={<VideoDetails />}/>
+            </Routes>
+        </div>
+      </Router>
     </AppContext>
   );
 };
